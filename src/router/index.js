5d82f66ea56f6940d1fa03474f882a7d05099b5a
haojiabin <991782946@qq.com>
2020-04-26 14:05:03 +0800

    登录、退出，以及地图（未完善）功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,43 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Map from '../components/Map'
+import vueJsonp from 'vue-jsonp'
+import BaiduMap from 'vue-baidu-map'
+import Home from '../components/home'
 
+Vue.use(BaiduMap, {
+  /* 申请的百度密钥，可以在百度地图官网申请 */
+  ak: 'brdBS22T3Ic5zeAqnqHXGisSWhQNU91P'
+})
+Vue.use(vueJsonp)
 Vue.use(VueRouter)
 
+
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path:'/',redirect: '/Login'},
+  { path: '/Login',component: Login},
+  {path: '/Map',component: Map},
+  {path: '/home',component: Home}
 ]
+
 
 const router = new VueRouter({
   routes
 })
+
+//挂载路由导航守卫
+router.beforeEach((to,from,next)=>{
+  //to 要访问的路径
+  //from 从哪个路径跳转而来
+  //next() 放行到一个路径
+  if (to.path==='/Login') return next()
+  //获取token
+  const tokenStr=  sessionStorage.getItem('token')
+  if (!tokenStr) return next('/Login')
+  //放行
+  next()
+})
+
 
 export default router
