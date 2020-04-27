ce1b895a838dd5948d1c571542707f008fd98c1f
hjb15822434196 <53111055+hjb15822434196@users.noreply.github.com>
2020-04-27 17:46:01 +0800

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,10 +12,12 @@
     <!--页面主体-->
     <el-container>
       <!--侧边栏-->
-      <el-aside width="200px">
-        <el-menu background-color="#dfdfe0" text-color="#fff" active-text-color="#48a2ff">
+      <el-aside :width = "isCollapse? '64px'  :  '200px' ">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 开启路由定位:router="true"-->
+        <el-menu background-color="#dfdfe0" text-color="black" active-text-color="#48a2ff" unique-opened :collapse="isCollapse" :collapse-transition="false" :router="true">
           <!--一级菜单--><!--index只能接受String-->
-          <el-submenu :index="item.id +'' "  v-for="item in menuList " :key="item.id" >
+          <el-submenu :index = "item.id + '' "  v-for="item in menuList " :key="item.id" >
             <template slot="title">
               <!--图标-->
               <i :class="iconsList[item.id]"></i>
@@ -23,7 +25,8 @@
               <span>{{item.authName}}</span>
             </template>
             <!--二级菜单-->
-            <el-menu-item :index="subItem.id" v-for="subItem in item.children" :key="subItem.id">
+            <!--路由自动定位地址:index =" '/'+subItem.path + '' "-->
+            <el-menu-item :index =" '/'+subItem.path + '' " v-for="subItem in item.children" :key="subItem.id">
               <!--图标-->
               <i class="el-icon-menu"></i>
               <!--文本-->
@@ -33,7 +36,10 @@
         </el-menu>
       </el-aside>
       <!--主页-->
-      <el-main>Main</el-main>
+      <el-main>
+        <!-- 路由占位符-->
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 
@@ -53,26 +59,32 @@
           '101' :'el-icon-shopping-bag-1',
           '102' :'el-icon-eleme',
           '145' :'el-icon-pie-chart'
-        }
+        },
+        //是否折叠
+        isCollapse : false
       }
     },
     created(){
       this.getMenuList()
     },
-     methods:{
+    methods:{
       //退出功能
-    logout(){
-      //清除token
-      window.sessionStorage.clear();
-      this.$router.push("/Login");
-    },
-       //获取所有菜单数据
-   async getMenuList(){
-   const {data : res} = await this.$http.get('menus')
-     if (res.meta.status != '200')  return this.$message.error(res.meta.msg)
-     this.menuList=res.data
-   }
-  }
+      logout(){
+        //清除token
+        window.sessionStorage.clear();
+        this.$router.push("/Login");
+      },
+      //获取所有菜单数据
+      async getMenuList(){
+        const {data : res} = await this.$http.get('menus')
+        if (res.meta.status != '200')  return this.$message.error(res.meta.msg)
+        this.menuList=res.data
+      },
+      //侧边栏折叠效果
+      toggleCollapse(){
+        this.isCollapse = !this.isCollapse;
+      }
+    }
   }
 </script>
 
@@ -95,6 +107,9 @@
   }
   .el-aside{
     background-color: #dfdfe0;
+    .el-menu{
+      border-right:none;
+    }
   }
   .el-main{
     background-color: #ffffff;
@@ -106,5 +121,13 @@
     height: 50px;
     width: 50px;
   }
-
+  .toggle-button{
+    background-color: #909399;
+    font-size: 10px;
+    line-height: 24px;
+    color: #fff;
+    text-align: center;
+    letter-spacing: 0.3em;
+    cursor: pointer;
+  }
 </style>
