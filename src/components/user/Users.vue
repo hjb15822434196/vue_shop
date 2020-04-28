c0ebd50bbfae3f92652caacff795e68c45749f9a
haojiabin <991782946@qq.com>
2020-04-28 22:56:27 +0800

    搜索用户（清空后查询全部）

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -10,8 +10,8 @@
     <el-card >
      <!-- 搜索区域-->
       <el-row :gutter="20">
-        <el-col :span="7"> <el-input placeholder="请输入内容" >
-          <el-button slot="append" icon="el-icon-search"></el-button>
+        <el-col :span="7"> <el-input v-model="queryInfo.query" clearable @clear="getUserList" placeholder="请输入内容" >
+          <el-button  slot="append" icon="el-icon-search" @click="getUserList"></el-button>
         </el-input></el-col>
         <el-col :span="4"> <el-button type="primary">添加用户</el-button></el-col>
       </el-row>
