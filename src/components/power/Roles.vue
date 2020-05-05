76ea5021a2ab30dddf4bc7b2dd66c4b703c838f1
haojiabin <991782946@qq.com>
2020-05-05 11:57:26 +0800

    角色列表删除功能

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -17,10 +17,10 @@
           <!--展开列-->
           <el-table-column type="expand">
             <template v-slot="scope">
-              <el-row class="vcenter" :class="['bdbottom',index1===0? 'bdtop':'']"  v-for="(item1,index1) in scope.row.children" :key="item1.id">
+              <el-row class="vcenter" :class="['bdbottom',index1===0? 'bdtop':'']"  v-for="(item1,index1) in scope.row.children" :key="item1.id" >
                 <!--渲染一级菜单-->
                 <el-col :span="5">
-                  <el-tag>{{item1.authName}}</el-tag>
+                  <el-tag closable @close="removeRightById(scope.row,item1.id)">{{item1.authName}}</el-tag>
                   <i class="el-icon-caret-right"></i>
                 </el-col>
                 <!--渲染二级和三级菜单-->
@@ -28,12 +28,12 @@
                   <el-row  class="vcenter" :class="[index2===0? '':'bdtop']" v-for="(item2,index2) in item1.children" :key="item2.id">
                     <!--二级-->
                     <el-col :span="6">
-                      <el-tag type="success">{{item2.authName}}</el-tag>
+                      <el-tag type="success" closable @close="removeRightById(scope.row,item2.id)">{{item2.authName}}</el-tag>
                       <i class="el-icon-caret-right"></i>
                     </el-col>
                     <!--三级-->
                     <el-col :span="18">
-                      <el-tag type="warning" v-for="(item3,index3) in item2.children" :key="item3.id">{{item3.authName}}</el-tag>
+                      <el-tag type="warning" v-for="(item3,index3) in item2.children" :key="item3.id" closable @close="removeRightById(scope.row,item3.id)">{{item3.authName}}</el-tag>
                     </el-col>
                   </el-row>
                 </el-col>
@@ -223,6 +223,28 @@
           this.$message.success('删除成功');
           //返回新的数据
           this.getRolesList()
+
+        },
+        //根据id删除对应的权限
+       async removeRightById(role,rightId){
+          const confirmResult=await this.$confirm('此操作将永久删除该权限, 是否继续?', '提示',
+            {
+              confirmButtonText: '确定',
+              cancelButtonText: '取消',
+              type: 'warning'
+            }).catch(err =>err);
+
+          if(confirmResult!=='confirm'){
+            return this.$message.info('已取消删除')
+          }
+          //请求删除
+          const{data:res}=  await this.$http.delete(`roles/${role.id}/rights/${rightId}`);
+          if (res.meta.status!==200){
+            return this.$message.error('删除用户权限失败')
+          }
+          this.$message.success('删除成功');
+         //返回新的数据
+        role.children=res.data
 
         }
       }
