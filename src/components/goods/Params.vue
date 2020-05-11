e5d318f65643012ef7d34b785fd7f3328dbc2653
haojiabin <991782946@qq.com>
2020-05-11 21:06:07 +0800

    获取静态、动态参数的卡片视图以及添加对话框

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -34,15 +34,63 @@
         <!--tab页签区-->
         <el-tabs v-model="activeName" @tab-click="handleClick">
           <el-tab-pane label="动态参数" name="many">
-            <el-button type="primary" size="mini" :disabled="isBtnDisabled">添加参数</el-button>
+            <el-button type="primary" size="mini" :disabled="isBtnDisabled" @click="dialogVisible=true">添加参数</el-button>
+            <!--动态参数列表-->
+            <el-table :data="manyTableData" border stripe>
+              <!--展开行-->
+              <el-table-column type="expand"></el-table-column>
+              <el-table-column type="index"></el-table-column>
+              <el-table-column label="参数列表" prop="attr_name"></el-table-column>
+              <el-table-column label="操作">
+                <template v-slot="scope">
+                  <!--编辑-->
+                  <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+                  <!--删除-->
+                  <el-button type="danger" icon="el-icon-delete" size="mini" >删除</el-button>
+                  </template>
+              </el-table-column>
+
+             </el-table>
           </el-tab-pane>
 
           <el-tab-pane label="静态属性" name="only">
-            <el-button type="primary" size="mini" :disabled="isBtnDisabled">添加属性</el-button>
+            <el-button type="primary" size="mini" :disabled="isBtnDisabled" @click="dialogVisible=true">添加属性</el-button>
+            <!--静态参数列表-->
+            <el-table :data="onlyTableData" border stripe>
+              <!--展开行-->
+              <el-table-column type="expand"></el-table-column>
+              <el-table-column type="index"></el-table-column>
+              <el-table-column label="参数列表" prop="attr_name"></el-table-column>
+              <el-table-column label="操作">
+                <template v-slot="scope">
+                  <!--编辑-->
+                  <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+                  <!--删除-->
+                  <el-button type="danger" icon="el-icon-delete" size="mini" >删除</el-button>
+                </template>
+              </el-table-column>
+
+            </el-table>
           </el-tab-pane>
         </el-tabs>
-
       </el-card>
+      <!--对话框-->
+      <el-dialog
+        :title="'添加'+titleText"
+        :visible.sync="dialogVisible"
+        width="50%"
+        @close="handleClose">
+        <!--提交表单-->
+        <el-form :model="addForm" :rules="addRules" ref="addFormRef" label-width="100px">
+          <el-form-item :label=titleText prop="attr_name">
+            <el-input v-model="addForm.attr_name"></el-input>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+  </span>
+      </el-dialog>
     </div>
 </template>
 
@@ -55,6 +103,16 @@
            activeName:'many',//被激活的标签
            manyTableData:[],//动态参数数据
            onlyTableData:[],//静态参数数据
+           dialogVisible:false,//控制对话框显隐
+           addForm:{
+             attr_name:''
+           },
+           addRules:{
+             attr_name:[
+               { required: true, message: '请输入参数名称', trigger: 'blur' },
+               { min: 3, max: 10, message: '长度在 3 到10 个字符', trigger: 'blur' }
+             ]
+           }
          }
        },
       created(){
@@ -67,6 +125,13 @@
             return true
           }
          return false
+        },
+        //动态计算标题文本
+        titleText(){
+          if (this.activeName==='many') {
+            return '动态参数'
+          }
+          return'静态属性'
         }
       },
       methods:{
@@ -110,6 +175,9 @@
           }else{
             this.onlyTableData=res.data
           }
+        },
+        handleClose(){
+          this.$refs.addFormRef.resetFields();
         }
       }
 
